d31f0f3d3a9975b7faca721e00bc406f5145557e
HeartBrokenGod <neelwanky@gmail.com>
2021-03-05 17:35:47 +0530

    go-neel branch created

--- neel/loops.go
+++ neel/loops.go
@@ -2,5 +2,29 @@ package neel
 import "fmt"
 
 func Loops(){
-	fmt.Println("Hello Loops")
+	i := 0
+
+	//	while loop
+	for i < 10 {
+		fmt.Println("while  i = ", i)
+		i++
+	}
+
+	//	do while loop
+	for {
+		fmt.Println("do while i = ",i)
+		if(i>10){
+			continue
+		}
+		break
+	}
+
+
+	//	for loop
+	for i = 0; i< 10; i++{
+		fmt.Println("for i = ",i)
+	}
+
+
+
 }
